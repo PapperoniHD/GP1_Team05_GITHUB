679441ffef8bef4f4dd04ed220a7912488273b3a
WhatTheMagic <robin.liljegren@edu.futuregames.nu>
2022-10-19 13:58:34 +0200

    Scores fixed (playtest needed)

--- GP1_Team05_URP/Assets/ScoreTextScript.cs
+++ GP1_Team05_URP/Assets/ScoreTextScript.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 using TMPro;
 public class ScoreTextScript : MonoBehaviour
 {
-    public float score;
-    private TMP_Text scoreText;   
-    
+    public float score1;
+    public float score2;
+    private TMP_Text scoreText1;
+    private TMP_Text scoreText2;
+
+    [SerializeField] private ScoreStorage _scrStrg;
+
     // Start is called before the first frame update
     void Start()
     {
         
-        scoreText = GameObject.Find("Score").GetComponent<TMP_Text>();
+        scoreText1 = GameObject.Find("Score1").GetComponent<TMP_Text>();
+        scoreText2 = GameObject.Find("Score2").GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        score = GameObject.Find("ScoreManager").GetComponent<Score>().playerOneScore;
-        scoreText.SetText(score.ToString("F0"));
+        score1 = _scrStrg.playerOneScore;
+        scoreText1.SetText(score1.ToString("F0"));
+
+        score2 = _scrStrg.playerTwoScore;
+        scoreText2.SetText(score2.ToString("F0"));
     }
     
 
